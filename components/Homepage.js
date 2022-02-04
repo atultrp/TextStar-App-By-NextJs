8a1e8d33d4bc3f339d3e57fda8ffa025acaed806
atultrp <atul31815@gmail.com>
2022-02-04 16:00:55 +0530

    Fixing Bugs

--- components/Homepage.js
+++ components/Homepage.js
@@ -41,15 +41,15 @@ const Homepage = () => {
             <h1 className="text-4xl my-2 text-[#11324D] font-medium">Enter the text to analyze below <ShortTextIcon fontSize="large" /> </h1>
             <textarea cols="30" rows="10" className="form-control block w-full px-3 py-1.5 text-base font-normal text-[#11324D] bg-white bg-clip-padding border border-solid border-teal-400 rounded transition ease-in-out m-0 focus:text-gray-700 :bg-white focus:border-teal-900 focus:outline-none shadow-md shadow-[#11324D]/50" value={text} onChange={handleOnChange}></textarea>
             <div>
-                <button class="bg-[#143c5c] hover:bg-[#11324D] text-white font-bold py-2 px-4 rounded-xl mr-3 mt-4 transition ease-in hover:-translate-y-1 hover:scale-110 duration-200" onClick={handleUpClick}>Uppercase</button>
+                <button className="bg-[#143c5c] hover:bg-[#11324D] text-white font-bold py-2 px-4 rounded-xl mr-3 mt-4 transition ease-in hover:-translate-y-1 hover:scale-110 duration-200" onClick={handleUpClick}>Uppercase</button>
 
-                <button class="bg-[#143c5c] hover:bg-[#11324D] text-white font-bold py-2 px-4 rounded-xl mr-3 mt-4 transition ease-in hover:-translate-y-1 hover:scale-110 duration-200" onClick={handleLowClick}>Lowercase</button>
+                <button className="bg-[#143c5c] hover:bg-[#11324D] text-white font-bold py-2 px-4 rounded-xl mr-3 mt-4 transition ease-in hover:-translate-y-1 hover:scale-110 duration-200" onClick={handleLowClick}>Lowercase</button>
 
-                <button class="bg-[#143c5c] hover:bg-[#11324D] text-white font-bold py-2 px-4 rounded-xl mr-3 mt-4 transition ease-in hover:-translate-y-1 hover:scale-110 duration-200" onClick={handleRemoveSpace}>Remove Extra Spaces</button>
+                <button className="bg-[#143c5c] hover:bg-[#11324D] text-white font-bold py-2 px-4 rounded-xl mr-3 mt-4 transition ease-in hover:-translate-y-1 hover:scale-110 duration-200" onClick={handleRemoveSpace}>Remove Extra Spaces</button>
 
-                <button class="bg-[#143c5c] hover:bg-[#11324D] text-white font-bold py-2 px-4 rounded-xl mr-3 mt-4 transition ease-in hover:-translate-y-1 hover:scale-110 duration-200" onClick={handleCopy}>Copy Text</button>
+                <button className="bg-[#143c5c] hover:bg-[#11324D] text-white font-bold py-2 px-4 rounded-xl mr-3 mt-4 transition ease-in hover:-translate-y-1 hover:scale-110 duration-200" onClick={handleCopy}>Copy Text</button>
 
-                <button class="bg-[#143c5c] hover:bg-[#11324D] text-white font-bold py-2 px-4 rounded-xl mr-3 mt-4 transition ease-in hover:-translate-y-1 hover:scale-110 duration-200" onClick={handleClearClick}>Clear Text</button>
+                <button className="bg-[#143c5c] hover:bg-[#11324D] text-white font-bold py-2 px-4 rounded-xl mr-3 mt-4 transition ease-in hover:-translate-y-1 hover:scale-110 duration-200" onClick={handleClearClick}>Clear Text</button>
             </div>
 
             <div className="my-3">
