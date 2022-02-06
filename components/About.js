121af30f682fb14cd4ea50439d5713f10b52973a
atultrp <atul31815@gmail.com>
2022-02-06 21:01:52 +0530

    Updated

--- components/About.js
+++ components/About.js
@@ -5,18 +5,18 @@ export default function About() {
     return (
         <div className="mx-5 my-3 md:mx-40">
                 <h1 className="mb-3 text-center text-2xl my-3 text-[#11324D] font-medium md:text-3xl">About Us</h1>
-                <p className="text-center">
+                <p className="text-center md:text-lg">
                     Learn more about the <strong>TextStar</strong>, who created this, what's its features are and what techs are included.
                 </p>
                 <div>
-                    <h2 className="font-medium my-3 text-[#11324D] text-lg">Introduction</h2>
-                    <p className=" ">
+                    <h2 className="font-medium my-3 text-[#11324D] text-xl">Introduction</h2>
+                    <p className="md:text-lg">
                         Basically, <strong>TextStar</strong> is a word counter and a character counting utility which can be used to manipulate your text in the way you want. You can remove extra spaces, copy the manipulated text as well as convert your text from uppercase to lowercase and lowercase to uppercase. And provided Minute Count time to read the text. Here is one more feature to preview all the text that user enters.
                     </p>
                 </div>
                 <div>
-                    <h2 className="font-medium my-3 text-[#11324D] text-lg">Tech Included</h2>
-                    <p className="lead ">
+                    <h2 className="font-medium my-3 text-[#11324D] text-xl">Tech Included</h2>
+                    <p className="md:text-lg">
                         <ul className="list-disc mx-6">
                             <li>HTML</li>
                             <li>NextJs</li>
@@ -26,8 +26,8 @@ export default function About() {
                     </p>
                 </div>
                 <div>
-                    <h2 className="font-medium my-3 text-[#11324D] text-lg">Creator</h2>
-                    <p className="lead">
+                    <h2 className="font-medium my-3 text-[#11324D] text-xl">Creator</h2>
+                    <p className="md:text-lg">
                         Hey there, I'm <strong>Atul Tripathi</strong> from India.
                         My passion for software lies with dreaming up ideas and making them come true with elegant interfaces. I take great care in the experience, architecture, and code quality of the things I build.
                         <br />
@@ -36,8 +36,8 @@ export default function About() {
                 </div>
                 <br />
                 <div>
-                    <h2 className="font-medium my-3 text-[#11324D] text-lg">Last but not Least!</h2>
-                    <p className="lead">
+                    <h2 className="font-medium my-3 text-[#11324D] text-xl">Last but not Least!</h2>
+                    <p className="md:text-lg">
                         How to reach me -
                         <ul className=" my-3 flex">
                             <li className="my-1">
@@ -63,7 +63,7 @@ export default function About() {
                             
                         </ul>
                         Created by using coding skills with love 💙️ by - <a href="https://github.com/atultrp">
-                        <button type="button" className="btn btn-light">Atul Tripathi</button>
+                        <button type="button">Atul Tripathi</button>
                         </a>
                     </p>
                 </div>
